d3ea251eaab0576db0f640bdbd28ce7ce837d693
Hariom Sagitra <hariomdhakad750910@gmail.com>
2025-11-28 11:42:06 +0530

    Add files via upload

---
+++ javaScript/class6/H.js
@@ -0,0 +1,31 @@
+
+//Higher order Function Example
+
+function hariom() {
+  console.log("Hariom Dhakad");
+  
+}
+
+
+function malkhan() {
+    console.log("Malkhan Singh");   
+}
+malkhan(hariom());
+
+
+// console.log(function(){console.log("IIFe Function") }());
+
+
+
+// Callback Function Example
+
+// function Ritik(){
+//   console.log("Ritik Singh");
+// }
+
+// function ankit(){
+//   console.log("Ankit Kumar");
+  
+// }
+
+// ankit(Ritik());
