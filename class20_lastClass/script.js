82233ce40ce84f82214e5eb616dd278923a700f7
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-01-16 14:23:14 +0530

    Add files via upload

---
+++ class20_lastClass/script.js
@@ -0,0 +1,11 @@
+let bodyele = document.createElement("div")
+console.log(bodyele)
+
+bodyele.setAttribute("id","div-1")
+
+let divEle = document.createElement("button")
+console.log(divEle)
+
+divEle.innerText = "hriomdhakad"
+
+.appendChild("body")
